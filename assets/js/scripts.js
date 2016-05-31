4501745675540ed30139a6acf23d26a3d9907685
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-05-30 22:00:14 -0400

    final touches

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,31 +1,47 @@
-// // When clicked, the main headers display a paragraph below them.
-// MY CODE ATTEMPTS BELOW
-// //BELOW:create a variable named clk.  Its value will be the class called "BigHeadings", from the document.
+
+//                 MY CODE ATTEMPTS BELOW
+// create a variable named clk.  Its value will be the class called
+// "BigHeadings", from the document.
 // var clk = document.getElementsByClassName('BigHeadings');
+// create a variable named para.  Its value will be the class called
+// "HiddenParagraphs", from the document.
 // var para = document.querySelector('HiddenParagraphs');
 //
-// // for (var 1=0;1<bigHeadings.length;i++){
+//  for (var 1=0;1<bigHeadings.length;i++){
 //
-//
-// clk.addEventListener('click', function() { //for this new clk variable, "listen" for whenever a user clicks
-//
+// clk.addEventListener('click', function() {
+//for this new clk variable, "listen" for whenever a user clicks
 //         };
 
-// THE CODE FROM JUSTIN'S REDDIT MENTOR:
+// THE CODE THAT I ENDED UP USING BELOW IS FROM Travis' REDDIT MENTOR http://codepen.io/birjolaxew/pen/ZOEBGv?editors=1010:
 
+// Create a variable named accordItems. Set its value to be any element with
+// the class name of AccordionItems
 var accordItems = document.getElementsByClassName("AccordionItems");
-
+// a for loop below- Start looking through the document for this variable "accordItems".
+//  whenever one is clicked on, run the following function:
 for (var i = 0; i < accordItems.length; ++i) {
   accordItems[i].addEventListener("click", function(){
+// We'll need two more variables for this function. The value of the variable
+// "ParagraphSmallHeaders" is assigned the value of the next element's class in
+//the document directly below AccordionItems, which is also named "ParagraphSmallHeaders".
+// The value of the variable "currentStyle" is assigned its value by looking in the
+// currently open the window, and finding the CSS style for the class
+// "ParagraphSmallHeaders"
 
     var ParagraphSmallHeaders = this.nextElementSibling;
     var currentStyle = window.getComputedStyle(ParagraphSmallHeaders);
-    //accordion show/hide controls
-    if(currentStyle.display === 'none'){ //content is hidden
-      ParagraphSmallHeaders.style.display = 'initial';//content is shown
-    } else {
+  // If the display is set to none in CSS, run this function.
+    if(currentStyle.display === 'none'){
+// for the variable ParagraphSmallHeaders, set the display to
+//  "initial" in CSS.  (which makes it visible - why?? Why use the word initial??)
+      ParagraphSmallHeaders.style.display = 'initial';
+    }
+    // otherwise, if the display is NOT set to none in CSS, then set it to none
+    // now, which makes it invisible.
+    else {
       ParagraphSmallHeaders.style.display = 'none';
-      //content is hidden
+
     };
  });
 }
