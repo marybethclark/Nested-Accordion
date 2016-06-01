a7a91aff4a90f35a051b88381b081e23bd93239c
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-06-01 16:28:36 -0400

    mission accomplished

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,1 +1,19 @@
-//test
+$(".Oatcake").on("click", function() {
+  $(".accordionOatcake").toggle();
+});
+
+$(".Cream").on("click", function() {
+  $(".accordionCream").toggle();
+});
+
+$(".Jelly").on("click", function() {
+  $(".accordionJelly").toggle();
+});
+
+$(".Danish").on("click", function() {
+  $(".accordionDanish").toggle();
+});
+
+$(".Jujubes").on("click", function() {
+  $(".accordionJujubes").toggle();
+});
