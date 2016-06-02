fffb87d6f1e37fb6d8b6a9127532a32ed2504ac8
MARY BETH CLARK <marybeth.clark@icloud.com>
2016-06-01 22:24:57 -0400

    still working on down arrow function

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -17,3 +17,40 @@ $(".Danish").on("click", function() {
 $(".Jujubes").on("click", function() {
   $(".accordionJujubes").toggle();
 });
+
+// $(".Jelly").on("click", function() {
+//   $(".accordionJelly").toggle();
+// });
+//
+// $(".Danish").on("click", function() {
+//   $(".accordionDanish").toggle();
+// });
+//
+// $(".Jujubes").on("click", function() {
+//   $(".accordionJujubes").toggle();
+// });
+//
+// $(".Oatcake").on("click", function() {
+//   $(".fa-arrow-downOatcake").toggle();
+//     $(".fa-arrow-right Oatcake").toggle();
+// });
+//
+// $(".Cream").on("click", function() {
+//   $(".fa-arrow-downCream").toggle();
+//     $(".fa-arrow-rightCream").toggle();
+// });
+//
+// $(".Jelly").on("click", function() {
+//   $(".fa-arrow-downJelly").toggle();
+//     $(".fa-arrow-rightJelly").toggle();
+// });
+//
+// $(".Danish").on("click", function() {
+//   $(".fa-arrow-downDanish").toggle();
+//     $(".fa-arrow-rightDanish").toggle();
+// });
+//
+// $(".Jujubes").on("click", function() {
+//   $(".fa-arrow-downJujubes").toggle();
+//     $(".fa-arrow-rightJujubes").toggle();
+// });
